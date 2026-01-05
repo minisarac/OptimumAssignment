08acfed28e05bce0a767991a31ea236a034e9864
deniz <minisarac@gmail.com>
2026-01-05 17:16:25 -0500

    Introducing trending period enum

---
+++ OptimumAssignment/TrendingPeriod.swift
@@ -0,0 +1,14 @@
+//
+//  TrendingPeriod.swift
+//  OptimumAssignment
+//
+//  Created by Deniz Sarac on 1/5/26.
+//
+
+import Foundation
+
+enum TrendingPeriod: String, CaseIterable {
+  case today = "Today"
+  case thisWeek = "This Week"
+
+}
