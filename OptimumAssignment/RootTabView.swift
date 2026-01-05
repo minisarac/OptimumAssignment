c32c09da18160de1497bfee8b6b29c033e77c06a
deniz <minisarac@gmail.com>
2026-01-05 15:30:57 -0500

    Creating the root tab view

---
+++ OptimumAssignment/RootTabView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  OptimumAssignment
+//
+//  Created by Deniz Sarac on 1/5/26.
+//
+
+import SwiftUI
+
+struct RootTabView: View {
+    var body: some View {
+      TabView {
+        HomeView()
+          .tabItem {
+            Label("Home", systemImage: "house")
+          }
+        
+        
+        FavoritesView()
+          .tabItem {
+            Label("Favorites", systemImage: "heart")
+          }
+        
+        
+        SettingsView()
+          .tabItem {
+            Label("Settings", systemImage: "gear")
+          }
+        
+      }
+      
+    }
+}
+
+#Preview {
+    RootTabView()
+}
