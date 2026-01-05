019cf95c9db3aac68584fef723ccdf5fbf2d4c41
deniz <minisarac@gmail.com>
2026-01-05 15:30:39 -0500

    Creating the app struct

---
+++ OptimumAssignment/OptimumAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  OptimumAssignmentApp.swift
+//  OptimumAssignment
+//
+//  Created by Deniz Sarac on 1/5/26.
+//
+
+import SwiftUI
+
+@main
+struct OptimumAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RootTabView()
+        }
+    }
+}
